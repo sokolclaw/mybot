ca2671f22d87e848ec87637b0b8ccedeb9ef6aa2
sokolclaw <kristoferiork@yandex.ru>
2023-05-23 18:00:08 +0500

    Add words counter and foolmoon date search

--- bot.py
+++ bot.py
@@ -26,6 +26,20 @@ def where_planet(update, context):
     constellation = ephem.constellation(planet)
     print(constellation)
     update.message.reply_text(constellation)
+
+def how_many_words(update, context):
+    user_text = update.message.text.split()
+    if len(user_text) == 1:
+        update.message.reply_text('В этом сообщении нет слов')
+    else:
+        update.message.reply_text(len(user_text) - 1)
+    print(len(user_text))
+    
+def when_fool_moon(update, context):
+    user_text = update.message.text.split()
+    data = user_text[1].split('.')
+    moon = ephem.next_full_moon(f'{data[2]}-{data[1]}-{data[0]}')
+    update.message.reply_text(moon)
     
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
@@ -33,6 +47,8 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('planet', where_planet))
+    dp.add_handler(CommandHandler('wordcount', how_many_words))
+    dp.add_handler(CommandHandler('next_fool_moon', when_fool_moon))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
